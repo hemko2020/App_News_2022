eb580fd979d7f7fa0db33b6004b8c7e6a4a7a4fe
hemko2020 <dihuer.hermann@icloud.com>
2022-02-24 16:30:06 +0100

    First co

---
+++ lib/Models-Views/Model_Views_Page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class ModelViewPages extends StatelessWidget {
+  const ModelViewPages({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/Services/Service_API.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ServiceAPIPage extends StatelessWidget {
+  const ServiceAPIPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
